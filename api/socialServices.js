e45a378b446998de7330b0a43ff3595048b58295
sirkorgo <sirkorgo@sirkorgo.com>
2026-06-14 01:09:33 -0700

    add passive aggressive message

--- api/socialServices.js
+++ api/socialServices.js
@@ -1,4 +1,4 @@
-const BLOCKED_IPS = ['76.176.19.211'];
+const BLOCKED_IPS = [/* '76.176.19.211',*/'2601:5cc:4801:d60:a14c:bd66:f217:f4b0','2603:6000:aff0:4b60:d87a:192f:f44c:14b9','2601:5cc:4801:d60:98cf:5550:7eaf:f3ac','172.118.3.253','2603:8001:99f0:5040:503c:334f:13a1:d3e'];
 
 export default function handler(req, res) {
   const forwarded = req.headers['x-forwarded-for'];
