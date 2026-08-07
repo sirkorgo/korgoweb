a4424829918f6912ff7112ce1ea57145fd7f6245
sirkorgo <sirkorgo@sirkorgo.com>
2026-08-07 16:56:43 -0700

    added SEO-facing sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
+  site: "https://www.sirkorgo.com",
+  integrations: [
+    sitemap({
+      filter: (page) => !/\/\d{4}\/\d{2}\/\d{2}\//.test(page),
+    }),
+  ],
 });
