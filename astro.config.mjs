98a3a8805e2836abe3536fc759ac5679a2a678ab
sirkorgo <sirkorgo@sirkorgo.com>
2026-08-07 13:46:24 -0700

    vercel setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import vercel from '@astrojs/vercel';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel()
+  output: "static",
 });
